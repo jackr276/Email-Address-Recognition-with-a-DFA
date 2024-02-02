73e765ec1a5c20f9b16cea501cfc79997f8cd6de
jackr276 <jack_m_robbins@me.com>
2024-02-02 11:19:14 -0500

    begin initial programming work

---
+++ src/p1_24s_010_jmr226.cpp
@@ -0,0 +1,15 @@
+/**
+ * Jack Robbins
+ * UCID: jmr226
+ * CS-341 Section 010
+ * Spring Semester, 2024
+*/
+
+#include<stdio.h>
+#include <string.h>
+
+
+int main(){
+    printf("%s/n%s/n%s/n%s/n", "Project 1 for CS 341", "")
+
+}
