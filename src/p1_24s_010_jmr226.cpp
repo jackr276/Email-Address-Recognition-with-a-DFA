b537368a33bef65d04d44c3038e82cd5fa72f4b2
jackr276 <jack_m_robbins@me.com>
2024-02-04 12:09:46 -0500

    now accepting number of strings to process

--- src/p1_24s_010_jmr226.cpp
+++ src/p1_24s_010_jmr226.cpp
@@ -8,9 +8,20 @@
 #include<stdio.h>
 #include <string.h>
 
+using namespace std;
 
 int main(){
+    //Initial message
     printf("%s\n%s\n%s\n%s\n%s\n", "Project 1 for CS 341", "Section Number: 010", "Semester: Spring 2024", "Written by: Jack Robbins, jmr226", "Instructor: Marvin Nakayama, marvin@njit.edu");
+
+    //Prompt user for number of strings to enter
+    printf("Enter the number of strings that you would like to process: ");
+    int numStrings;
+
+    scanf("%d", &numStrings);
+
+    printf("%d\n", numStrings);
+    
 
     return 0;
 }
