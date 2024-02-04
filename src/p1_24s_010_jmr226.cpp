c7c13fd61fb3006db74d1eaf11c5e46c4e5cc5c3
jackr276 <jack_m_robbins@me.com>
2024-02-04 12:36:43 -0500

    added enum states

--- src/p1_24s_010_jmr226.cpp
+++ src/p1_24s_010_jmr226.cpp
@@ -7,8 +7,14 @@
 
 #include<stdio.h>
 #include <string.h>
+#include <sstream>
 
 using namespace std;
+
+bool dfa_226(string w);
+
+//All DFA states needed -> Note: q1 start state, q10 trap state
+enum dfa_states_226 {q1, q2, q3, q4, q5, q6, q7, q8, q9, q10};
 
 int main(){
     //Initial message
@@ -23,10 +29,19 @@ int main(){
     printf("Processing %d%s%s\n", numStrings, " string", numStrings == 1 ? "" : "s");
 
     //Enter the main loop unless numStrings is 0
-    for(int i = 0; i < numStrings && numStrings != 0; i++){
-        printf("%d", i);
+    for(int i = 1; i <= numStrings && numStrings != 0; i++){
+        printf("Enter string %d of %d: ", i, numStrings);
+        string word;
+        scanf("%s", &word);
+
+        dfa_226(word);
+        
     }
     
-
     return 0;
+}
+
+
+bool dfa_226(string w){
+    return false;
 }
