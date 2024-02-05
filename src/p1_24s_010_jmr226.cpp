e206a2afce343bca00f1c932cb17f1b6d1639244
jackr276 <jack_m_robbins@me.com>
2024-02-04 20:09:40 -0500

    update printing style

--- src/p1_24s_010_jmr226.cpp
+++ src/p1_24s_010_jmr226.cpp
@@ -26,7 +26,7 @@ string phi = "@";
 
 int main(){
     //Initial message
-    printf("%s\n%s\n%s\n%s\n%s\n", "Project 1 for CS 341", "Section Number: 010", "Semester: Spring 2024", "Written by: Jack Robbins, jmr226", "Instructor: Marvin Nakayama, marvin@njit.edu");
+    printf("\n%s\n%s\n%s\n%s\n%s\n\n", "Project 1 for CS 341", "Section Number: 010", "Semester: Spring 2024", "Written by: Jack Robbins, jmr226", "Instructor: Marvin Nakayama, marvin@njit.edu");
 
     //Prompt user for number of strings to enter
     printf("Enter the number of strings that you would like to process: ");
